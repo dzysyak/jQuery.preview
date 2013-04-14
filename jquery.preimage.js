4b0d6716651f65bf61e49837aa7f321da0fa9110
Sergiy Dzysyak <dzysyak@gmail.com>
2013-04-14 03:53:50 +0300

    New options were added to improve selected images scaling.

--- jquery.preimage.js
+++ jquery.preimage.js
@@ -1,7 +1,8 @@
 (function( $ ){
   var settings = {
   		'prefix': 'prev_',
-		'types': ['image/gif', 'image/png', 'image/jpeg']
+		'types': ['image/gif', 'image/png', 'image/jpeg'],
+		'mime': {'.jpe': 'image/jpeg', '.jpeg': 'image/jpeg', '.jpg': 'image/jpeg', '.gif': 'image/gif', '.png': 'image/png', '.x-png': 'image/png', '.tif': 'image/tiff', '.tiff': 'image/tiff'}
 	};
 
   var methods = {
@@ -18,24 +19,88 @@
 			$(this).unbind('change');
 		})
      },
-     change : function(event) { 
+     base64_encode: function(data) {
+		// http://kevin.vanzonneveld.net
+		// +   original by: Tyler Akins (http://rumkin.com)
+		// +   improved by: Bayron Guevara
+		// +   improved by: Thunder.m
+		// +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+		// +   bugfixed by: Pellentesque Malesuada
+		// +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+		// +   improved by: Rafał Kukawski (http://kukawski.pl)
+		// *     example 1: base64_encode('Kevin van Zonneveld');
+		// *     returns 1: 'S2V2aW4gdmFuIFpvbm5ldmVsZA=='
+		// mozilla has this native
+		// - but breaks in 2.0.0.12!
+		//if (typeof this.window['btoa'] == 'function') {
+		//    return btoa(data);
+		//}
+		var b64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+		var o1, o2, o3, h1, h2, h3, h4, bits, i = 0,
+		ac = 0,
+		enc = "",
+		tmp_arr = [];
+
+		if (!data) {
+		return data;
+		}
+
+		do { // pack three octets into four hexets
+		o1 = data.charCodeAt(i++);
+		o2 = data.charCodeAt(i++);
+		o3 = data.charCodeAt(i++);
+
+		bits = o1 << 16 | o2 << 8 | o3;
+
+		h1 = bits >> 18 & 0x3f;
+		h2 = bits >> 12 & 0x3f;
+		h3 = bits >> 6 & 0x3f;
+		h4 = bits & 0x3f;
+
+		// use hexets to index into b64, and append result to encoded string
+		tmp_arr[ac++] = b64.charAt(h1) + b64.charAt(h2) + b64.charAt(h3) + b64.charAt(h4);
+		} while (i < data.length);
+
+		enc = tmp_arr.join('');
+
+		var r = data.length % 3;
+
+		return (r ? enc.slice(0, r - 3) : enc) + '==='.slice(r || 3);
+
+	},
+    change : function(event) { 
      	var id = this.id
      	
      	$('#'+settings['prefix']+id).html('');
      	
      	for(i=0; i<this.files.length; i++){
-     		if(!$.inArray(this.files[i].type, settings['types'])){
+     		if(!$.inArray(this.files[i].type, settings['types']) == -1){
      			window.alert("File of not allowed type");	
      			return false
      		}
      	}
      	
-     	for(i=0; i<this.files.length; i++){
-     		var reader = new FileReader();
-			reader.onload = function (e) {
-				$('<img />').attr({'src': e.target.result, 'alt': 'Image preview' }).addClass(settings['prefix']+'thumb').appendTo($('#'+settings['prefix']+id));
-			};
-			reader.readAsDataURL(this.files[i]);
+     	if(window.FileReader){
+     	    for(i=0; i<this.files.length; i++){
+     	    	var reader = new FileReader();
+	    		reader.onload = function (e) {
+	    			//$('<img />').attr({'src': e.target.result, 'alt': 'Image preview' }).addClass(settings['prefix']+'thumb').appendTo($('#'+settings['prefix']+id));
+	    			$('<div />').css({'bacground-image': ('url('+e.target.result+')'), 'background-repeat': 'no-repeat', 'background-size': 'cover' }).addClass(settings['prefix']+'thumb').appendTo($('#'+settings['prefix']+id));
+	    		};
+	    		reader.readAsDataURL(this.files[i]);
+     	    }
+     	}else{
+     		var ForReading = 1, ForWriting = 2;
+     		var fso = new ActiveXObject("Scripting.FileSystemObject");
+     		
+     		for(i=0; i<this.files.length; i++){
+				f = fso.OpenTextFile(this.files[i], ForReading);
+				var cnt = (f.AtEndOfStream)?"":f.ReadAll();
+     			f.Close();
+     			var ext = fso.GetExtensionName(this.files[i]);
+     			//$('<img />').attr({'src': 'data:image/'+ext+';'+this.base64_encode(cnt), 'alt': 'Image preview' }).addClass(settings['prefix']+'thumb').appendTo($('#'+settings['prefix']+id));
+     			$('<div />').css({'bacground-image': ("url('data:"+this.mime[ext]+";"+e.target.result+"')"), 'background-repeat': 'no-repeat', 'background-size': 'cover' }).addClass(settings['prefix']+'thumb').appendTo($('#'+settings['prefix']+id));
+     	    }
      	}
      }
   };
